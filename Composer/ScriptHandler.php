6bb4845f3b9484ff44e2b54a13b49e720fd6d61c
Hidabe <fhidalgo@sopinet.com>
2013-05-20 08:30:43 +0200

    Trying composer script

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ */
+
+namespace Sopinet\Bundle\BootstapExtendBundle\Composer;
+
+use Composer\Script\CommandEvent;
+
+/**
+ */
+class ScriptHandler
+{
+    /**
+     *
+     * @param $event CommandEvent A instance
+     */
+    public static function copyExport(CommandEvent $event)
+    {
+			global $kernel;
+			if ( 'AppCache' == get_class($kernel) )
+			{
+				 $kernel = $kernel->getKernel();
+			}
+			$path = $kernel->getRootDir();
+			echo $path;
+			exit();
+		}
+}
